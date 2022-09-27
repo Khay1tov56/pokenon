8ef293fcbc0d45aa100fd2e9566536c323655287
NurilloOkeng5670 <khayitov5670@gmail.com>
2022-09-27 16:40:23 +0300

    Add  remote

---
+++ js/masala.js
@@ -0,0 +1,45 @@
+//  masala -1
+
+// let arr = prompt("Kiriting")
+// var1 = arr.length;
+// var2 = arr.replace(/[aeiouAEIOU]/gi, "")
+// console.log((arr = Number(var1) - Number(var2.length)));
+
+
+//  masala -2 
+
+
+// const ages = [5, 16, 7, 4, 10, 3, 4, 4, 14, 5, 14, 4, 44, 4];
+// let quti = []
+
+// for(var age of ages) {
+//     if(age != 4) {
+//         quti.push(age)
+//     }
+// }
+
+// console.log(quti);
+
+
+
+// Masala -3
+
+// let newArray = [1, 6, 2, 5, 4, 9, 3, 7, 8];
+// console.log(newArray);
+// for (var array of newArray) {
+//     if (array % 2 !== 0) {
+//         array -= 1;
+//     }else {
+//         array += 1;
+
+//     }
+
+//     console.log(array);
+// }
+
+
+// Masala -4
+
+// let newArray = [1, 6, 2, 5, 4, 9, 3, 7,18];
+// var letNum = newArray[0] + newArray[newArray.length -1];
+// console.log(letNum);
