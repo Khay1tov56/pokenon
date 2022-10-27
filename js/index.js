4547e937930ba0df67131f74f3cbfcbab662c00c
Khay1tov/56 <nurillohayitov7@gmail.com>
2022-10-27 16:47:35 +0300

    Add remote

--- js/index.js
+++ js/index.js
@@ -6,77 +6,7 @@ let elSortSelect = document.querySelector(".genres-select");
 const genres = [];
 
 
-
-function createList(pokes, mark) {
-  elList.innerHTML = "";
-
-
-  let newFragment = document.createDocumentFragment();
-  for (let poke of pokes) {
-    let newItem = document.createElement("li");
-    let newImg = document.createElement("img");
-    let newText = document.createElement("p")
-    let newSpan = document.createElement("span")
-    newImg.src = poke.img;
-    newText.textContent = poke.name;
-    newSpan.textContent = poke.weight;
-    newItem.appendChild(newImg);
-    newItem.appendChild(newText);
-    newItem.appendChild(newSpan);
-    newFragment.appendChild(newItem);
-
-  }
-  elList.appendChild(newFragment);
-
-}
-
-// Search qiluvchi funksiya
-function filreterdFn(arr) {
-  return pokemons.filter(item => item.name.match(arr))
-}
-
-elForm.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-
-  let searchElement = new RegExp(elInput.value, "gi");
-  let absd = filreterdFn(searchElement);
-  
-  if (absd.length > 0) {
-    sorting(absd, elSelect.value);
-    pokeKg(absd, elSelect.value);
-    createList(absd);
-  
-  } else {
-    alert("Movie not found");
-  }
-  
-})
-createList(pokemons);
-elInput.value = "";
-
-// Sort qiluvchi fn
-
-function sorting(item, type) {
-  if (type === "aazz") {
-    item.sort((a, b) => a.name.charCodeAt(0) - b.name.charCodeAt(0));
-  } else if (type === "zzaa") {
-    item.sort((a, b) => b.name.charCodeAt(0) - a.name.charCodeAt(0));
-
-  }
-};
-
-function pokeKg (item, kg) {
-  if(Number(kg === "kgstart")) {
-    item.sort((a, b) => a.weight.charCodeAt(0) - b.weight.charCodeAt(0));
-  } else if (Number(kg === "kgend")) {
-    item.sort((a, b) => b.weight.charCodeAt(0) - a.weight.charCodeAt(0))
-  }
-}
-
-sorting()
-pokeKg()
-
-
+// Arrayga oid genres
 function genresList() {
 
   pokemons.forEach(film => {
@@ -106,16 +36,83 @@ function showMoviesGenresOption() {
   elSortSelect.appendChild(newSelectFragment);
 }
 
+function createList(pokes) {
+  elList.innerHTML = "";
+
+
+  let newFragment = document.createDocumentFragment();
+  for (let poke of pokes) {
+    let newItem = document.createElement("li");
+    newItem.classList.add("col-3", "bg-secondary","text-white", "my-5", "py-4")
+    let newData = document.createElement("p");
+    let newImg = document.createElement("img");
+    let newText = document.createElement("h3");
+    let newSpan = document.createElement("span");
+    let newCandy = document.createElement("p");
+    newImg.src = poke.img;
+    newText.textContent = poke.name;
+    newSpan.textContent = poke.weight;
+    newData.textContent = poke.height;
+    newCandy.textContent = poke.weaknesses;
+    newItem.appendChild(newImg);
+    newItem.appendChild(newText);
+    newItem.appendChild(newData);
+    newItem.appendChild(newSpan);
+    newItem.appendChild(newCandy);
+    newFragment.appendChild(newItem);
+
+  }
+  elList.appendChild(newFragment);
+
+}
+
 function showSearchMovies(items) {
 
   return pokemons.filter(movie => {
     
-    const meetsCriteria = movie.title.match(items) && elSortSelect.value === "All" ||
-    movie.categories.includes(elSortSelect.value);
+    const meetsCriteria = movie.name.match(items) && (elSortSelect.value === "All" ||
+    movie.weaknesses.includes(elSortSelect.value));
 
     return meetsCriteria;
   });
 }
 
+function sortingAll(sortedList, select) {
+  if (select === "aazz") {
+    sortedList.sort((a,b) => a.name.localeCompare(b.name))
+  }else if(select === "zzaa") {
+    sortedList.sort((a,b) => b.name.localeCompare(a.name))
+
+  }else if (select === "kgstart") {
+    sortedList.sort((a,b) => a.weight.localeCompare(b.weight))
+
+  }else if (select === "kgend") {
+    sortedList.sort((a,b) => b.weight.localeCompare(a.weight))
+  }else if (select === "hgstart") {
+    sortedList.sort((a,b) => a.height.localeCompare(b.height))
+  }else if (select === "hgend") {
+    sortedList.sort((a,b) => b.height.localeCompare(a.height))
+  }
+}
+
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  let searchElement = new RegExp(elInput.value, "gi");
+  let absd = showSearchMovies(searchElement);
+  
+  if (absd.length > 0) {
+    sortingAll(absd, elSelect.value);
+    createList(absd);
+  } else {
+    alert("Movie not found");
+  }
+  
+})
+createList(pokemons);
+elInput.value = "";
+
+sortingAll()
 genresList()
-showMoviesGenresOption();
+showMoviesGenresOption()
+
